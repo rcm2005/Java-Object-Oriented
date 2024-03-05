e9e05fc4b3ca6cd61633c55138451c22bb9ef704
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-05 10:54:41 -0300

    adicionando getters and setters

--- Aula 6/Encapsulamento]/src/RadioEncapsulada.java
+++ Aula 6/Encapsulamento]/src/RadioEncapsulada.java
@@ -1,6 +1,35 @@
 public class RadioEncapsulada {
-    public static void main(String[] args) {
-        
+
+    private int volume;
+    private float estacao;
+
+    public int getVolume(){
+        return volume;
+    }
+
+    public void setVolume(int volume){
+        this.volume = volume;
+
+    }
+    public float getEstacao(){
+        return estacao;
+
+    }
+    public void setEstacao(float estacao){
+        this.estacao = estacao;
+    }
+
+    public void aumentarVolume(){
+        volume ++;
+    }
+
+    public void diminuirVolume(){
+        volume --;
+    }
+    public void mostrar(){
+        System.out.println("Volume: " + volume + "\nEstação: " + estacao);
     }
     
 }
+    
+
