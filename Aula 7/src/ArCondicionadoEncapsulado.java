19dc6aee766386533d1fe35ff09a27200d709289
rafaelmattos <rcm2005@icloud.com>
2024-03-07 09:36:57 -0300

    adicionando aula 7, exercicios

---
+++ Aula 7/src/ArCondicionadoEncapsulado.java
@@ -0,0 +1,73 @@
+public class ArCondicionadoEncapsulado {
+
+    public int temperatura;
+    public String modo;
+
+    //getters and setters
+
+    public int getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(int temperatura) {
+        try {
+            if (temperatura >= 15 && temperatura <= 26){
+                this.temperatura = temperatura;
+            }
+               
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        
+    }
+
+    public String getModo() {
+        return modo;
+    }
+
+
+    
+    public void setModo(String modo) {
+       try {
+               
+        if (modo.equals("Ventilar") || modo.equals("Aquecer") || modo.equals("Resfriar")){
+            this.modo = modo;
+        }
+       } catch (Exception e) {
+        System.out.println(e.getMessage());
+       }
+
+        
+    }
+    // Métodos
+
+    public void aumentarTemperatura(){
+        if (temperatura < 26){
+            temperatura ++;
+        }
+    }
+
+    public void diminuirTemperatura(){
+        if (temperatura > 15){
+            temperatura --;
+        }
+    }
+
+    public void trocarModo(String modo){
+        
+        try {
+            if (modo.equals("Ventilar") || modo.equals("Aquecer") || modo.equals("Resfriar")){
+                this.modo = modo;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        
+    }
+
+    public void mostrar(){
+        System.out.println("a temperatua esta em: " + temperatura +" e o modo é: " + modo );
+    }
+}
