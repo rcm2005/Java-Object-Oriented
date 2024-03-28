86cea3af04d3aed05e8b8d1de23aea5ad5bf5e5a
rafaelmattos <rcm2005@icloud.com>
2024-03-28 14:51:31 -0300

    adicionando exercicio aula 12 Eleitor com construtor

---
+++ aula 12/Eleitor/src/Eleitor.java
@@ -0,0 +1,42 @@
+public class Eleitor {
+    
+    
+    private String nome;
+    private int anoNascimento, numTitulo, zona, secao;
+
+
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public int getNumTitulo() {
+        return numTitulo;
+    }
+    public int getSecao() {
+        return secao;
+    }
+    public int getZona() {
+        return zona;
+    }
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setNumTitulo(int numTitulo) {
+        this.numTitulo = numTitulo;
+    }
+    public void setSecao(int secao) {
+        this.secao = secao;
+    }
+    public void setZona(int zona) {
+        this.zona = zona;
+    }
+    
+    public int calculaIdade(int anoAtual){
+
+    }
+}
