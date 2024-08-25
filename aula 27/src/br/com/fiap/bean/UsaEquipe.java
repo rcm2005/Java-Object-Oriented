b49ed7894d1a63cb4e8e72f9d74975c41be3f053
rcm2005 <rcm2005@icloud.com>
2024-08-25 16:45:12 -0300

    exemplo de uso collections arraylist

--- aula 27/src/br/com/fiap/bean/UsaEquipe.java
+++ aula 27/src/br/com/fiap/bean/UsaEquipe.java
@@ -1,17 +1,36 @@
 package br.com.fiap.bean;
 
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 public class UsaEquipe {
     public static void main(String[] args) {
         Euquipe grupo;
         String aux, nome, escolha = "sim";
         while (escolha.equalsIgnoreCase("sim")) {
             try {
+                nome = JOptionPane.showInputDialog("Dê um nome para a equipe");
+                String membros = "continua";
+                ArrayList<String> integrantes = new ArrayList<String>();
+                while (membros.equalsIgnoreCase("continua")) {
+                    aux = JOptionPane.showInputDialog("Digite integrante ou 'fim' para encerrar:");
+                    if (aux.equals("fim")){
+                        membros = "fim";
+                    }else{
+                        integrantes.add(aux);
+                    }
+                    grupo = new Equipe(nome, integrantes);
+                    grupo.listaEquipe();
+                    escolha = JOptionPane.showInputDialog("Deseja continuar ?");
+                }
                 
             } catch (Exception e) {
-                // TODO: handle exception
+                JOptionPane.showMessageDialog(null, e.getMessage());
             }
             
         }
+        JOptionPane.showMessageDialog(null, "Fim do programa. Até breve!");
     }
     
 }
