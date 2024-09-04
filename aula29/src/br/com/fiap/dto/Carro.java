382cadc9064eb719f93482ed751c12d47f3aca03
rcm2005 <rcm2005@icloud.com>
2024-09-04 10:42:10 -0300

    adicionando classe carro

---
+++ aula29/src/br/com/fiap/dto/Carro.java
@@ -0,0 +1,13 @@
+package br.com.fiap.dto;
+
+public class Carro {
+    private String placa;
+    private String cor;
+    private String descricao;
+
+
+    public String getPlaca(){
+        return placa;
+    }
+
+}
